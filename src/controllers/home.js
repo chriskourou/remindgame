b3887f150994b8b07e4af9cc75072431f5fe5f7d
Christos Kouroupetroglou <chris.kourou@gmail.com>
2021-01-01 21:31:33 +0100

    Adding second page

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,5 @@
 import { testEnvironmentVariable } from '../settings';
 
 export const indexPage = (req, res) => res.status(200).json({ message: testEnvironmentVariable });
+
+export const secondIndexPage = (req, res) => res.status(200).json({ message: "HAHA" });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { indexPage } from '../controllers';
+import { indexPage, secondIndexPage } from '../controllers';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
+indexRouter.get('/2', secondIndexPage);
 
 export default indexRouter;
