7a77aac239fb3edf104f57e706e1e0f39d219fdb
Christos Kouroupetroglou <chris.kourou@gmail.com>
2021-01-02 16:08:04 +0100

    Added insert message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { indexPage, secondIndexPage, messagesPage } from '../controllers';
+import { indexPage, secondIndexPage, messagesPage, addMessage } from '../controllers';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/2', secondIndexPage);
 indexRouter.get('/messages', messagesPage)
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
