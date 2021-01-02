4c02339879a94046747c9e3ebe64fbdd3b57c6b3
Christos Kouroupetroglou <chris.kourou@gmail.com>
2021-01-02 21:32:30 +0100

    ssl add

--- src/models/pool.js
+++ src/models/pool.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 import { connectionString } from '../settings';
 dotenv.config();
 
-export const pool = new Pool({ connectionString });
+export const pool = new Pool({ connectionString: connectionString, ssl: true});
